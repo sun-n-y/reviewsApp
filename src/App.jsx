d18aae2250cdabadfe632eca4c69ed06b87ffb27
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-25 14:24:00 -0400

    add data import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+import { useState } from 'react';
+import people from './data';
+
 const App = () => {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
   return <h2>Reviews Starter</h2>;
 };
 export default App;
