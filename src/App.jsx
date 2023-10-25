45c8daa83591fb0f761c4ba15efb30b9ea360c79
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-25 14:34:08 -0400

    add react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react';
 import people from './data';
-
+import { FaBeer } from 'react-icons/fa';
 const App = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
 
-  return <h2>Reviews Starter</h2>;
+  return (
+    <h2>
+      Reviews Starter <FaBeer />
+    </h2>
+  );
 };
 export default App;
