cd5fbbc773c3b0cd017249f1fe235fad40c2753a
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-25 14:40:01 -0400

    add render user

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react';
 import people from './data';
-import { FaBeer } from 'react-icons/fa';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 const App = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
 
   return (
-    <h2>
-      Reviews Starter <FaBeer />
-    </h2>
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+      </article>
+    </main>
   );
 };
 export default App;
